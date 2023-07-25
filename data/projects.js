0681abdf33677ad89a57964658c3beebdc79bb79
MARIA ISABEL LOPEZ GARCIA <isabelopez.cursos@gmail.com>
2023-07-25 14:19:43 +0200

    mensaje

---
+++ data/projects.js
@@ -0,0 +1,50 @@
+export const projects = [
+    {
+    title: "Primer proyecto",
+    description: "Réplica de la pagina de SLEEPTOKEN realizada con HTML y CSS",
+    image:
+    "./icons/SLEEPTOKEN.png",
+    tech: ["HTML5", "CSS3"],
+    github: "https://github.com/Isabellp2023",
+    link: "www.proyecto1.com",
+    },
+    {
+    title: "Segundo proyecto",
+    description: "Esta es la descripción de mi segundo proyecto",
+    image:
+    "https://images.unsplash.com/photo-1661961111184-11317b40adb2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&",
+    tech: ["React", "HTML5", "CSS3", "JavaScript"],
+    github: "http://github.com/myuser/proyecto2",
+    link: "www.proyecto1.com",
+    },
+    {
+    title: "Tercer proyecto",
+    description: "Esta es la descripción de mi tercer proyecto",
+    image:
+    "https://images.unsplash.com/photo-1669570094762-828f3dfaf675?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&",
+    tech: ["React", "HTML5", "CSS3", "JavaScript"],
+    github: "http://github.com/myuser/proyecto3",
+    link: "www.proyecto1.com",
+    },
+    {
+    title: "Cuarto proyecto",
+    description: "Esta es la descripción de mi cuarto proyecto",
+    image:
+    "https://images.unsplash.com/photo-1664575198308-3959904fa430?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&",
+    tech: ["React", "HTML5", "CSS3", "JavaScript"],
+    github: "http://github.com/myuser/proyecto4",
+    link: "www.proyecto1.com",
+    },
+    {
+    title: "Quinto proyecto",
+    description: "Esta es la descripción de mi quinto proyecto",
+    image:
+    "https://images.unsplash.com/photo-1665686306574-1ace09918530?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&",
+    tech: ["React", "HTML5", "CSS3", "JavaScript"],
+    github: "http://github.com/myuser/proyecto5",
+    link: "www.proyecto1.com",
+    },
+    ];
+    
+    
+   
